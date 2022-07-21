52b8344c04e5ef56724e5ec526f7e244e6b4b2aa
Marcin Wróbel <marcin.wrobel@ng-analytics.com>
2022-07-21 13:02:59 +0200

    #5: - spack package for Teuchos

--- var/spack/repos/builtin/packages/teuchos/package.py
+++ var/spack/repos/builtin/packages/teuchos/package.py
@@ -19,98 +19,76 @@
 #
 # See the Spack documentation for more information on packaging.
 # ----------------------------------------------------------------------------
-
-import sys
+import subprocess
 
 from spack import *
 
-#def trilinosez_depends_on_test(spec, **kwargs):
-#    depends_on('mpi')
 
 class Teuchos(CMakePackage):
     """FIXME: Put a proper description of your package here."""
 
     # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
-    git      = "ssh://git@github.com/MikolajZuzek/Trilinos.git"
-    url      = "http://localhost/0.1.0.tar.gz"
+    homepage = "http://localhost/"
+    url      = "http://localhost/teuchos-0.1.0.tar.gz"
 
     maintainers = ['keitat']
 
-    version('1-teuchos-cmake', branch='1-teuchos-cmake', preferred=True)
+    version('0.1.0', sha256=@@@@@, preferred=True)
 
     root_cmakelists_dir = "packages/teuchos"
 
     depends_on('cmake@3.20.0:', type='build')
+    depends_on('openblas')
+    depends_on('lapack')
+    depends_on('boost', when='+Boost')
+    depends_on('eigen', when='+Eigen')
+    depends_on('kokkos', when='+Kokkos')
+    depends_on('openmpi', when='+MPI')
 
-    # Mandatory TPL dependencies
-    # TPL (BLAS, LAPACK)
-    #    depends_on('openblas')
-
-    # Optionnal TPL dependencies(MPI, Eigen, BinUtils, QD, ARPREC, Boost, Qt)
-    variant('MPI', default=False, description='Compile with MPI parallelism')
-    variant('Eigen3', default=False, description='Compile with Eigen')
+    # Optionnal TPL dependencies
     variant('Boost', default=False, description='Compile with Boost')
-    variant('Pthread', default=False, description='Compile with Pthread')
+    variant('Eigen', default=False, description='Compile with Eigen')
     variant('Kokkos', default=False, description='Compile with Kokkos')
+    variant('MPI', default=False, description='Compile with MPI parallelism')
+    variant('Pthread', default=False, description='Compile with Pthread')
+    variant('Qt', default=False, description='Compile with Qt')
+    # Not yet supported in Cmake
     variant('ARPREC', default=False, description='Compile with ARPREC')
-    variant('QT', default=False, description='Compile with Qt')
-    variant('QD', default=False, description='Compile with QD')
     variant('BinUtils', default=False, description='Compile with BinUtils')
+    variant('QD', default=False, description='Compile with QD')
     variant('Valgrind', default=False, description='Compile with Valgrind')
 
-    # Deeper depdencies depending of Variants:
-    depends_on('boost', when='+Boost')
-    depends_on('kokkos', when='+Kokkos')
-    depends_on('eigen', when='+Eigen3')
-    depends_on('mpi', when='+MPI')
-
-    # Optionnal Package dependencies (Kokkos)
-
-    # Extra Cmake Options
-
-
     def cmake_args(self):
+        def define_teuchos_enable(suffix, value=None):
+            if value is None:
+                return self.define_from_variant(f'Teuchos_ENABLE_{suffix}', suffix)
+            elif isinstance(value, bool):
+                return self.define(f'Teuchos_ENABLE_{suffix}', value)
 
-        spec = self.spec
-        define = CMakePackage.define
-        define_from_variant = self.define_from_variant
+        num_of_proc = 1
+        try:
+            num_of_proc = int(subprocess.run(['nproc', '--all'],
+                                             capture_output=True, encoding='utf-8').stdout.replace('\n', ''))
+        except:
+            pass
 
-        def _make_definer(prefix):
-            def define_enable(suffix, value=None):
-                key = prefix + suffix
-                if value is None:
-                    # Default to lower-case spec
-                    value = suffix
-                elif isinstance(value, bool):
-                    # Explicit true/false
-                    return define(key, value)
-                return define_from_variant(key, value)
-            return define_enable
-
-        # Return "Teuchos_ENABLE_XXX" for spec "+xxx" or boolean value
-        define_teuchos_enable = _make_definer("Teuchos_ENABLE_")
-        # Same but for TPLs
-        define_tpl_enable = _make_definer("TPL_ENABLE_")
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
         args = [
-        '-DCMAKE_POSITION_INDEPENDENT_CODE=ON',
-        '-DTEUCHOS_STANDALONE_PACKAGE=ON'
+            '-DBUILD_SHARED_LIBS=ON',
+            f'-DMPI_EXEC_DEFAULT_NUMPROCS={num_of_proc}',
+            f'-DMPI_EXEC_MAX_NUMPROCS={num_of_proc}',
+            '-DTEUCHOS_HAS_TRILINOS=OFF',
+            '-DTeuchos_ENABLE_TESTS=ON'
         ]
-
         args.extend([
-            define_teuchos_enable('MPI'),
-            define_teuchos_enable('Eigen3'),
-            define_teuchos_enable('Boost'),
-            define_teuchos_enable('Pthread'),
-            define_teuchos_enable('Kokkos'),
-            define_teuchos_enable('QD'),
             define_teuchos_enable('ARPREC'),
-            define_teuchos_enable('QT'),
             define_teuchos_enable('BinUtils'),
+            define_teuchos_enable('Boost'),
+            define_teuchos_enable('Eigen'),
+            define_teuchos_enable('Kokkos'),
+            define_teuchos_enable('MPI'),
+            define_teuchos_enable('Pthread'),
+            define_teuchos_enable('QD'),
+            define_teuchos_enable('Qt'),
             define_teuchos_enable('Valgrind'),
         ])
-
         return args
