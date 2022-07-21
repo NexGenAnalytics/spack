05d3f9e6510243aaeb8472ea8cff60f5fd83fbb2
Marcin Wróbel <marcin.wrobel@ng-analytics.com>
2022-07-21 15:46:46 +0200

    #5: - back to proper version

--- var/spack/repos/builtin/packages/teuchos/package.py
+++ var/spack/repos/builtin/packages/teuchos/package.py
@@ -65,12 +65,6 @@ class Teuchos(CMakePackage):
             elif isinstance(value, bool):
                 return self.define(f'Teuchos_ENABLE_{suffix}', value)
 
-        def define_tpl_enable(suffix, value=None):
-            if value is None:
-                return self.define_from_variant(f'TPL_ENABLE_{suffix}', suffix)
-            elif isinstance(value, bool):
-                return self.define(f'TPL_ENABLE_{suffix}', value)
-
         num_of_proc = 1
         try:
             num_of_proc = int(subprocess.run(['nproc', '--all'],
@@ -86,15 +80,15 @@ class Teuchos(CMakePackage):
             '-DTeuchos_ENABLE_TESTS=ON'
         ]
         args.extend([
-            define_teuchos_enable('ARPREC'),
             define_teuchos_enable('Boost'),
             define_teuchos_enable('Eigen'),
             define_teuchos_enable('Kokkos'),
             define_teuchos_enable('MPI'),
             define_teuchos_enable('Pthread'),
-            define_teuchos_enable('QD'),
             define_teuchos_enable('Qt'),
-            define_tpl_enable('BinUtils'),
-            define_tpl_enable('Valgrind'),
+            define_teuchos_enable('ARPREC'),
+            define_teuchos_enable('BinUtils'),
+            define_teuchos_enable('QD'),
+            define_teuchos_enable('Valgrind'),
         ])
         return args
